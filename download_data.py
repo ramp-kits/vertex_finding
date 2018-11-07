50b40abf042939866f46cfe6acb38f7ca05c1e47
Florian Reiss <freiss@cern.ch>
2018-11-07 14:45:01 +0100

    download and extract data

--- download_data.py
+++ download_data.py
@@ -1,25 +1,44 @@
 from __future__ import print_function
 
 import os
+import tarfile
 
 try:
     from urllib.request import urlretrieve
 except ImportError:
     from urllib import urlretrieve
 
-URLBASE = 'https://storage.ramp.studio/mars_craters/{}'
+#helper function to unpack the data
+def unpack_tar(filename, type):
+    tar = tarfile.open(filename)
+    for member in tar.getmembers():
+      #check if member is file
+      if member.isreg():
+        name = member.name
+        if name.endswith('.json'):
+          #remove prefix
+          name = name.split('/')[1]
+          member.name = name
+          tar.extract(member, './data/' + type)
+    tar.close()
+
+
+URLBASE = 'https://storage.ramp.studio/vertex_finding/{}'
 DATA = [
-    'data_train.npy', 'data_test.npy']
-LABELS = [
-    'labels_train.csv', 'labels_test.csv']
+    '28kEvents.tar.gz', '30kEvents.tar.gz']
+
 
 
 def main(output_dir='data'):
-    filenames = DATA + LABELS
+    filenames = DATA 
     urls = [URLBASE.format(filename) for filename in filenames]
 
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
+    if not os.path.exists(output_dir +'/train'):
+        os.mkdir(output_dir +'/train')
+    if not os.path.exists(output_dir +'/test'):
+        os.mkdir(output_dir +'/test')
 
     # notfound = []
     for url, filename in zip(urls, filenames):
@@ -31,6 +50,9 @@ def main(output_dir='data'):
         print("Downloading from {} ...".format(url))
         urlretrieve(url, filename=output_file)
         print("=> File saved as {}".format(output_file))
+    print("Unpacking tar files. This may take a while, please be patient.")
+    unpack_tar('data/30kEvents.tar.gz', 'train')
+    unpack_tar('data/28kEvents.tar.gz', 'test')
 
 
 if __name__ == '__main__':
